8a76ebd095a68e7894b5c75bf532a40a558edbb2
Justina Molina <jmolina0095@gmail.com>
2024-04-03 14:50:52 -0700

    Added rudementary camera
    Former-commit-id: 0c39e66604e3a2da936dd2edacaf858829753446

---
+++ OpenVR-3DModelerTool/src/Renderer/Camera.h
@@ -0,0 +1,68 @@
+#include "glm/glm.hpp"
+#include "glm/gtc/quaternion.hpp"
+#include "glm/gtx/rotate_vector.hpp"
+#include "math.h"
+
+class Camera {
+public:
+    Camera(float x, float y, float z);
+
+    Camera(const glm::vec3& position,
+        const glm::vec3& up,
+        const glm::vec3& right,
+        const glm::vec3& view);
+
+    void OnRender();
+
+    void Rotate(float angle, float x, float y, float z);
+    void Rotate(glm::quat &q);
+    void RotateX(float radians);
+    void RotateY(float radians);
+    void RotateZ(float radians);
+    void Transform(float x, float y, float z);
+
+    void MoveForward(float distance);
+    void MoveUpwards(float distance);
+    void MoveSideways(float distance);
+
+
+    const glm::mat4 &ViewMatrix() const {
+        return m_ViewMatrix;
+    }
+
+    const glm::mat4 &ProjectionMatrix() const {
+        return m_ProjectionMatrix;
+    }
+
+    void SetUpVector(float x, float y, float z) {
+        m_Up.x = x;
+        m_Up.y = y;
+        m_Up.z = z;
+    }
+
+    void SetRightVector(float x, float y, float z) {
+        m_Right.x = x;
+        m_Right.y = y;
+        m_Right.z = z;
+    }
+
+    void SetViewVector(float x, float y, float z) {
+        m_View.x = x;
+        m_View.y = y;
+        m_View.z = z;
+    }
+
+private:
+    // Position
+    glm::vec3 m_Position;
+    glm::mat4 m_ViewMatrix;
+    glm::mat4 m_ProjectionMatrix;
+
+    // Orientation
+    glm::vec3 m_Up;
+    glm::vec3 m_Right;
+    glm::vec3 m_View;
+
+    // Rotation
+    glm::vec3 m_Rotation;
+};
